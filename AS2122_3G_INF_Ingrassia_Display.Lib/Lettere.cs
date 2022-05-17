6f71536368490cd5a3a7f6d0242607b3a98ca2c0
Samuele Ingrassia <samuele.ingrassia@studenti.ittsrimini.edu.it>
2022-05-17 12:36:11 +0200

    Library: Visibilità Aumentata dei Caratteri senza doverli Mostrare sul Display

--- AS2122_3G_INF_Ingrassia_Display.Lib/Lettere.cs
+++ AS2122_3G_INF_Ingrassia_Display.Lib/Lettere.cs
@@ -4,37 +4,222 @@ namespace AS2122_3G_INF_Ingrassia_Display.Lib
 {
     public class Lettere
     {
-        public bool[,] letteraA = new bool[,] { { false, true, false }, { true, false, true }, { true, true, true }, { true, false, true }, { true, false, true } };
-        public bool[,] letteraB = new bool[,] { { true, true, false }, { true, false, true }, { true, true, false }, { true, false, true }, { true, true, false } };
-        public bool[,] letteraC = new bool[,] { { true, true, true }, { true, false, false }, { true, false, false }, { true, false, false }, { true, true, true } };
-        public bool[,] letteraD = new bool[,] { { true, true, false }, { true, false, true }, { true, false, true }, { true, false, true }, { true, true, false } };
-        public bool[,] letteraE = new bool[,] { { true, true, true }, { true, false, false }, { true, true, false }, { true, false, false }, { true, true, true } };
-        public bool[,] letteraF = new bool[,] { { true, true, true }, { true, false, false }, { true, true, false }, { true, false, false }, { true, false, false } };
-        public bool[,] letteraG = new bool[,] { { false, true, true }, { true, false, true }, { false, true, true }, { false, false, true }, { true, true, true } };
-        public bool[,] letteraH = new bool[,] { { true, false, true }, { true, false, true }, { true, true, true }, { true, false, true }, { true, false, true } };
-        public bool[,] letteraI = new bool[,] { { false, true, false }, { false, true, false }, { false, true, false }, { false, true, false }, { false, true, false } };
-        public bool[,] letteraJ = new bool[,] { { false, true, false }, { false, true, false }, { false, true, false }, { false, true, false }, { true, true, false } };
-        public bool[,] letteraK = new bool[,] { { true, false, true }, { true, true, false }, { true, false, false }, { true, true, false }, { true, false, true } };
-        public bool[,] letteraL = new bool[,] { { true, false, false }, { true, false, false }, { true, false, false }, { true, false, false }, { true, true, true } };
-        public bool[,] letteraM = new bool[,] { { true, false, true }, { true, true, true }, { true, false, true }, { true, false, true }, { true, false, true } };
-        public bool[,] letteraN = new bool[,] { { false, false, false }, { true, false, false }, { true, true, true }, { true, false, true }, { true, false, true } };
-        public bool[,] letteraO = new bool[,] { { true, true, true }, { true, false, true }, { true, false, true }, { true, false, true }, { true, true, true } };
-        public bool[,] letteraP = new bool[,] { { true, true, false }, { true, false, true }, { true, true, false }, { true, false, false }, { true, false, false } };
-        public bool[,] letteraQ = new bool[,] { { true, true, true }, { true, false, true }, { true, false, true }, { true, true, false }, { false, false, true } };
-        public bool[,] letteraR = new bool[,] { { true, true, false }, { true, false, true }, { true, true, false }, { true, false, true }, { true, false, true } };
-        public bool[,] letteraS = new bool[,] { { true, true, true }, { true, false, false }, { true, true, true }, { false, false, true }, { true, true, true } };
-        public bool[,] letteraT = new bool[,] { { true, true, true }, { false, true, false }, { false, true, false }, { false, true, false }, { false, true, false } };
-        public bool[,] letteraU = new bool[,] { { true, false, true }, { true, false, true }, { true, false, true }, { true, false, true }, { true, true, true } };
-        public bool[,] letteraV = new bool[,] { { true, false, true }, { true, false, true }, { true, false, true }, { true, false, true }, { false, true, false } };
-        public bool[,] letteraW = new bool[,] { { true, false, true }, { true, false, true }, { true, false, true }, { true, true, true }, { true, false, true } };
-        public bool[,] letteraX = new bool[,] { { true, false, true }, { true, false, true }, { false, true, false }, { true, false, true }, { true, false, true } };
-        public bool[,] letteraY = new bool[,] { { true, false, true }, { true, false, true }, { true, true, true }, { false, true, false }, { false, true, false } };
-        public bool[,] letteraZ = new bool[,] { { true, true, true }, { false, false, true }, { false, true, false }, { true, false, false }, { true, true, true } };
-        public bool[,] vuoto = new bool[,] { { false, false, false }, { false, false, false }, { false, false, false }, { false, false, false }, { false, false, false } };
-        public bool[,] esclamazione = new bool[,] { { false, true, false }, { false, true, false }, { false, true, false }, { false, false, false }, { false, true, false } };
-        public bool[,] interrogazione = new bool[,] { { true, true, true }, { false, false, true }, { false, true, true }, { false, false, false }, { false, true, false } };
-        public bool[,] apostrofo = new bool[,] { { true, false, false }, { true, false, false }, { false, false, false }, { false, false, false }, { false, false, false } };
-        public bool[,] virgola = new bool[,] { { false, false, false }, { false, false, false }, { false, true, false }, { false, true, false }, { true, false, false } };
+        public bool[,] letteraA = new bool[,] { 
+            { false, true, false }, 
+            { true, false, true }, 
+            { true, true, true }, 
+            { true, false, true }, 
+            { true, false, true } };
+      
+        public bool[,] letteraB = new bool[,] { 
+            { true, true, false }, 
+            { true, false, true }, 
+            { true, true, false }, 
+            { true, false, true }, 
+            { true, true, false } };
+       
+        public bool[,] letteraC = new bool[,] { 
+            { true, true, true }, 
+            { true, false, false }, 
+            { true, false, false }, 
+            { true, false, false }, 
+            { true, true, true } };
+      
+        public bool[,] letteraD = new bool[,] { 
+            { true, true, false }, 
+            { true, false, true }, 
+            { true, false, true }, 
+            { true, false, true }, 
+            { true, true, false } };
+       
+        public bool[,] letteraE = new bool[,] { 
+            { true, true, true }, 
+            { true, false, false }, 
+            { true, true, false }, 
+            { true, false, false }, 
+            { true, true, true } };
+      
+        public bool[,] letteraF = new bool[,] { 
+            { true, true, true }, 
+            { true, false, false },  
+            { true, true, false },  
+            { true, false, false },   
+            { true, false, false } };
+       
+        public bool[,] letteraG = new bool[,] { 
+            { false, true, true }, 
+            { true, false, true }, 
+            { false, true, true }, 
+            { false, false, true }, 
+            { true, true, true } };
+      
+        public bool[,] letteraH = new bool[,] { 
+            { true, false, true }, 
+            { true, false, true }, 
+            { true, true, true }, 
+            { true, false, true }, 
+            { true, false, true } };
+       
+        public bool[,] letteraI = new bool[,] { 
+            { false, true, false }, 
+            { false, true, false }, 
+            { false, true, false }, 
+            { false, true, false }, 
+            { false, true, false } };
+      
+        public bool[,] letteraJ = new bool[,] { 
+            { false, true, false }, 
+            { false, true, false }, 
+            { false, true, false }, 
+            { false, true, false }, 
+            { true, true, false } };
+      
+        public bool[,] letteraK = new bool[,] { 
+            { true, false, true }, 
+            { true, true, false }, 
+            { true, false, false }, 
+            { true, true, false }, 
+            { true, false, true } };
+      
+        public bool[,] letteraL = new bool[,] { 
+            { true, false, false }, 
+            { true, false, false }, 
+            { true, false, false }, 
+            { true, false, false }, 
+            { true, true, true } };
+       
+        public bool[,] letteraM = new bool[,] { 
+            { true, false, true }, 
+            { true, true, true }, 
+            { true, false, true }, 
+            { true, false, true }, 
+            { true, false, true } };
+       
+        public bool[,] letteraN = new bool[,] { 
+            { false, false, false }, 
+            { true, false, false }, 
+            { true, true, true }, 
+            { true, false, true }, 
+            { true, false, true } };
+       
+        public bool[,] letteraO = new bool[,] { 
+            { true, true, true }, 
+            { true, false, true }, 
+            { true, false, true }, 
+            { true, false, true }, 
+            { true, true, true } };
+       
+        public bool[,] letteraP = new bool[,] { 
+            { true, true, false }, 
+            { true, false, true }, 
+            { true, true, false }, 
+            { true, false, false }, 
+            { true, false, false } };
+        
+        public bool[,] letteraQ = new bool[,] { 
+            { true, true, true }, 
+            { true, false, true }, 
+            { true, false, true }, 
+            { true, true, false }, 
+            { false, false, true } };
+       
+        public bool[,] letteraR = new bool[,] { 
+            { true, true, false }, 
+            { true, false, true }, 
+            { true, true, false }, 
+            { true, false, true }, 
+            { true, false, true } };
+       
+        public bool[,] letteraS = new bool[,] { 
+            { true, true, true }, 
+            { true, false, false }, 
+            { true, true, true }, 
+            { false, false, true }, 
+            { true, true, true } };
+      
+        public bool[,] letteraT = new bool[,] { 
+            { true, true, true }, 
+            { false, true, false }, 
+            { false, true, false }, 
+            { false, true, false }, 
+            { false, true, false } };
+     
+        public bool[,] letteraU = new bool[,] { 
+            { true, false, true }, 
+            { true, false, true }, 
+            { true, false, true }, 
+            { true, false, true }, 
+            { true, true, true } };
+      
+        public bool[,] letteraV = new bool[,] { 
+            { true, false, true }, 
+            { true, false, true }, 
+            { true, false, true }, 
+            { true, false, true }, 
+            { false, true, false } };
+      
+        public bool[,] letteraW = new bool[,] { 
+            { true, false, true }, 
+            { true, false, true }, 
+            { true, false, true }, 
+            { true, true, true }, 
+            { true, false, true } };
+       
+        public bool[,] letteraX = new bool[,] { 
+            { true, false, true }, 
+            { true, false, true }, 
+            { false, true, false }, 
+            { true, false, true }, 
+            { true, false, true } };
+       
+        public bool[,] letteraY = new bool[,] { 
+            { true, false, true }, 
+            { true, false, true }, 
+            { true, true, true }, 
+            { false, true, false }, 
+            { false, true, false } };
+       
+        public bool[,] letteraZ = new bool[,] { 
+            { true, true, true }, 
+            { false, false, true }, 
+            { false, true, false }, 
+            { true, false, false }, 
+            { true, true, true } };
+       
+        public bool[,] vuoto = new bool[,] { 
+            { false, false, false }, 
+            { false, false, false }, 
+            { false, false, false }, 
+            { false, false, false }, 
+            { false, false, false } };
+        
+        public bool[,] esclamazione = new bool[,] { 
+            { false, true, false }, 
+            { false, true, false }, 
+            { false, true, false }, 
+            { false, false, false }, 
+            { false, true, false } };
+       
+        public bool[,] interrogazione = new bool[,] { 
+            { true, true, true }, 
+            { false, false, true }, 
+            { false, true, true }, 
+            { false, false, false }, 
+            { false, true, false } };
+        
+        public bool[,] apostrofo = new bool[,] { 
+            { true, false, false }, 
+            { true, false, false }, 
+            { false, false, false }, 
+            { false, false, false }, 
+            { false, false, false } };
+        
+        public bool[,] virgola = new bool[,] { 
+            { false, false, false }, 
+            { false, false, false }, 
+            { false, true, false }, 
+            { false, true, false }, 
+            { true, false, false } };
 
 
     }
